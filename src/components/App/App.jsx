d7d89ca5321ef087f223bfbea1475312c65c1dbf
andriy andrusenko <andrusenkone@gmail.com>
2024-12-10 21:12:21 +0200

    adds starting files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
